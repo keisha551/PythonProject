85201146f875c02cc5c2629b865774a5c55443c1
keisha551 <20251136@mywhitecliffe.com>
2025-08-06 15:15:32 +1200

    testingt

---
+++ class_practicals/cars.py
@@ -0,0 +1,12 @@
+cars=[]
+for i in range(5):
+    c=input("enter your model")
+    cars.append(c)
+print(cars)
+cars=[]
+i=0
+while i<5:
+    c=input("enter car model")
+    cars.append(c)
+    i+=1
+print(cars)
